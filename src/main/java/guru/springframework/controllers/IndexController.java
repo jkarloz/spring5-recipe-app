ea63553cce321351601be34a86d49ea600d1f67a
cancerberosoft1 <crisjherico@gmail.com>
2017-11-01 18:36:53 -0500

    Add one to one and one to many relationships

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,9 @@
+package guru.springframework.controllers;
+
+/**
+ * @author Juan Carlos - 2017/10/30
+ *
+ */
+public class IndexController {
+
+}
---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,63 @@
+package guru.springframework.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+/**
+ * @author Juan Carlos - 2017/10/30
+ *
+ */
+@Entity
+public class Ingredient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	private BigDecimal amount;
+	
+	@ManyToOne
+	private Recipe recipe;
+	
+	@OneToOne(fetch = FetchType.EAGER) //To be sure that this value will be retrieved from database each time is required
+	private UnitOfMeasure uom;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	public Recipe getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	public UnitOfMeasure getUom() {
+		return uom;
+	}
+	public void setUom(UnitOfMeasure uom) {
+		this.uom = uom;
+	}
+	
+}
