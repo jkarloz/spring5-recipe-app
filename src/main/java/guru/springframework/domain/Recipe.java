db0b52b9c4e691225fce41c711d97f4d815d4756
cancerberosoft1 <crisjherico@gmail.com>
2017-11-01 20:03:30 -0500

    Add relationship ManyToMany between recipe and category

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -9,7 +9,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -44,6 +47,12 @@ public class Recipe {
 	private Notes notes;	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe") // 'recipe' is target property in Ingredient class
 	private Set<Ingredient> ingredients;
+	
+	@ManyToMany
+	@JoinTable(name = "recipe_category", 
+	joinColumns = @JoinColumn(name = "recipe_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
 	
 	public Long getId() {
 		return id;
@@ -116,6 +125,12 @@ public class Recipe {
 	}
 	public void setIngredients(Set<Ingredient> ingredients) {
 		this.ingredients = ingredients;
+	}
+	public Set<Category> getCategories() {
+		return categories;
+	}
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
 	}
 
 }
