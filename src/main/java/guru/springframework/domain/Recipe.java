b81ea8883dd40aceb7dd7290129a00d68fe1230b
cancerberosoft1 <crisjherico@gmail.com>
2017-11-01 19:27:00 -0500

    Add code to use persistence wiht enumerations for Difficulty class

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -4,12 +4,16 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+
+import guru.springframework.enums.Difficulty;
 
 /**
  * @author Juan Carlos - 2017/10/30
@@ -32,6 +36,10 @@ public class Recipe {
 	
 	@Lob
 	private Byte[] image;
+	
+	@Enumerated(value = EnumType.STRING) //By default is ordinal, but we will use the string value in case enum values order change
+	private Difficulty difficulty;
+	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe") // 'recipe' is target property in Ingredient class
@@ -96,6 +104,18 @@ public class Recipe {
 	}
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 
 }
---
+++ src/main/java/guru/springframework/enums/Difficulty.java
@@ -0,0 +1,10 @@
+package guru.springframework.enums;
+
+/**
+ * @author Juan Carlos - 2017/11/01
+ *
+ */
+public enum Difficulty {
+	EASY, MODERATE, HARD;
+
+}
