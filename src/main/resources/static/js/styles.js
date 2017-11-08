821734a02feac2bb535ff5317348c7d9b0b85570
cancerberosoft1 <crisjherico@gmail.com>
2017-11-08 11:48:01 -0500

    Add styles to html page using Bootstrap js

---
+++ src/main/resources/static/js/styles.js
@@ -0,0 +1,9 @@
+//alert('JS file loaded!');
+
+      $( document ).ready(function() {
+    	  $("#recipeslist").addClass("table table-hover table-bordered");
+    	  $("#headerdiv").addClass("panel-heading");
+    	  $("#bodydiv").addClass("panel-body");
+    	  $("h1").addClass("bg-primary");
+      });
+
