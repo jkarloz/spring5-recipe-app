955903cf095c7435657fdd938f6dc5ceb98ad931
cancerberosoft1 <crisjherico@gmail.com>
2017-11-07 21:06:06 -0500

    Modify code to enable lombok for logging using Slf4j annotation

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Service;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Juan Carlos - 2017/11/06
  *
  */
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -23,6 +25,7 @@ public class RecipeServiceImpl implements RecipeService {
 
 	@Override
 	public Set<Recipe> getRecipes() {
+		log.info("I'm in the service");
 		Set<Recipe> recipeSet = new HashSet<>();
 		recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
 		return recipeSet;
