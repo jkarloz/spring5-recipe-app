307ef5a997fa6796010bbbe1af41d70b5cd977ac
cancerberosoft1 <crisjherico@gmail.com>
2017-11-09 12:24:35 -0500

    Add unit test for controller calls using MockitoMvc

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -13,6 +16,8 @@ import org.junit.Test;
 import org.mockito.*;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import guru.springframework.domain.Recipe;
@@ -37,6 +42,14 @@ public class IndexControllerTest {
 		indexController = new IndexController(recipeService);
 	}
 
+	@Test
+	public void testMockMVC() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+		mockMvc.perform(get("/"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("index"));
+	}
+	
 	/**
 	 * Test method for {@link guru.springframework.controllers.IndexController#getIndexPage(org.springframework.ui.Model)}.
 	 */
