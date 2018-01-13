039907f1dce91f47a9875decf17a557589752086
cancerberosoft1 <crisjherico@gmail.com>
2018-01-12 20:09:34 -0500

    Add validations using JSR 303 in spring

--- src/main/java/guru/springframework/command/RecipeCommand.java
+++ src/main/java/guru/springframework/command/RecipeCommand.java
@@ -3,6 +3,11 @@ package guru.springframework.command;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.*;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
 import guru.springframework.enums.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,12 +23,28 @@ import lombok.Setter;
 @NoArgsConstructor
 public class RecipeCommand {
 	private Long id;
+	
+	@NotBlank
+	@Size(min = 3, max = 255)
 	private String description;
+	
+	@Min(1)
+	@Max(999)
 	private Integer prepTime;
+	
+	@Min(1)
+	@Max(999)
 	private Integer cookTime;
+	
+	@Min(1)
+	@Max(100)
 	private Integer servings;
 	private String source;
+	
+	@URL
 	private String url;
+	
+	@NotBlank
 	private String directions;
 	private Byte[] image;
 	private Difficulty difficulty;
--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,8 +1,11 @@
 package guru.springframework.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -51,7 +54,16 @@ public class RecipeController {
     }
 
 	@PostMapping("recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+	public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand recipeCommand, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			bindingResult.getAllErrors().forEach(objectError -> {
+				log.debug(objectError.toString());
+			});
+			
+			return "recipe/recipeform";
+		}
+		
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
 		return "redirect:/recipe/" + savedCommand.getId() + "/show/" ;
 	}
